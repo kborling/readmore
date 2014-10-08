d86224ae1ae06b8b308bee18556cc4ab4496a154
Kevin Borling <kborling@gmail.com>
2014-10-08 14:29:50 -0700

    Completed Testing of Read More Plugin

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
   
   // Your Scripts
 
+  $('article').readmore({
+  speed: 175,
+  maxHeight: 510,
+  moreLink: '<a href="#">Read more..</a>',
+  lessLink: '<a href="#">Read less..</a>'
+});
+
 });
